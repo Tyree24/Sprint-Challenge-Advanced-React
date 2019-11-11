6bed70491fc4899c7c9f51e017aa20969611f9f8
Tyree Lebeau <tyreelebeau@Tyrees-iMac.local>
2019-11-10 22:29:18 -0600

    Created some pretty ugly boxes but they're getting fixed

--- client/src/Components/PlayersList.js
+++ client/src/Components/PlayersList.js
@@ -1,18 +1,19 @@
 import React from "react";
 import PlayerCard from './PlayerCard';
+import { makeStyles, Box } from '@material-ui/core';
 
 const PlayerList = ({ players }) => {
 
     if(players.length) {
 
     return (
-
-        <div> 
+<Box display='flex' flexDirection='column' alignItems='center'>
 
 {players.map(player => {
-                return <PlayerCard player={player} key={player.id} />
-                })}
-             </div>
+    return <PlayerCard player={player} key={player.id} />
+    })}
+</Box>
+        
     );
 } else {
     return(
