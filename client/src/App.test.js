a96a10df1cb6da5c4d2c3b263f8e85c1f1f73497
Tyree Lebeau <tyreelebeau@Tyrees-iMac.local>
2019-11-11 22:33:46 -0600

    second attempt at mvp

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {render} from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('Navbar contains text', () => {
+  const { getByText } = render(<App />);
+  getByText(/World Cup/i);
+});
+
+test('is there a something dark', () => {
+  const { getByTestId  } = render(<App />);
+  getByTestId (/dark/i);
 });
